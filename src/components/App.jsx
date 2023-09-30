8342ec7b031b8e4eab192577ab7d12c8b2438967
MaksymSkala <maksymskala@gmail.com>
2023-09-30 20:11:21 +0300

    add Friend/Transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import Profile from '../components/Profile'; // Правильний шлях до компонента Profile
-import Statistics from '../components/Statistics'; // Правильний шлях до компонента Statistics
-import data from '../data.json'; // Правильний шлях до data.json
+import Profile from '../components/Profile';
+import Statistics from '../components/Statistics';
+import data from '../data.json';
+import FriendList from './FriendList';
+import friendsData from '../friends.json';
+import TransactionHistory from './TransactionHistory';
+import transactions from '../transactions.json';
 import './styles.css';
 
 const App = () => {
   return (
     <div>
       <Profile />
-      <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />    
+      <FriendList friends={friendsData} />
+      <TransactionHistory items={transactions} />
     </div>
+    
   );
 }
 
